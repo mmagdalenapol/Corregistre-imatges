0f95818080169d09cfb0e3a287bd0b9e2c28d03e
mmagdalenapol <mariamagdalenapolpujadas@gmail.com>
2019-10-03 14:29:30 +0200

    canvis funcio anhir

--- spline_registration/databases.py
+++ spline_registration/databases.py
@@ -15,14 +15,41 @@ def anhir_test():
         'expert_information': None,
     }
 
+#torna vertader si es carpeta
+def mirar_carpeta(pathcarpeta):
+    return os.path.isdir(pathcarpeta)
+
+
 def anhir():
     carpeta_inici = base_path('databases/anhir/dataset_medium')
     contingut_carpeta_inici=os.listdir(carpeta_inici)#llista de les carpetes i arxius que hi ha dins la nostra carpeta d'inici
+    direccions=[]
+    i=-1
     for subcarpeta in contingut_carpeta_inici:
-        if os.path.isdir(carpeta_inici+'/'+subcarpeta) == True:# Si és una carpeta haurem de seguir cercant dins la carpeta i aquesta comanda
+        i=i+1
+        l = []
+        direccio=carpeta_inici + '/' + subcarpeta
+        j=-1
+
+        while mirar_carpeta(direccio) == True:# Si és una carpeta haurem de seguir cercant dins la carpeta i aquesta comanda
+            x=os.listdir(direccio)
+            for element in x:
+                l.append(direccio+'/'+element)
+             #ara a l tenim els path de de totes les subcarpetes de x i ara hem de mirar dins elles
+            j=j+1
+            direccio= l[j]
+
+        direccions.append(l)
+
+    return direccions
+
+
 
    #ara per cada subcarpeta trobada hauré de mirar dedins i així fins que arribi a que ja no es una carpeta que per tant serà una imatge i
     #guardaré la seva direcció de manera que després hi pugui accedir
+
+
+
 
 
 
