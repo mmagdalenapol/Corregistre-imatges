a82e86af276c370124f927ff67c82da7b5416f07
mmagdalenapol <mariamagdalenapolpujadas@gmail.com>
2020-06-21 23:10:15 +0200

    proves

--- spline_registration/proves/coses_que_no.py
+++ spline_registration/proves/coses_que_no.py
@@ -98,4 +98,141 @@ for x in range (0, imatge.shape[0]):
             np.append(indexy, np.where(imatge2 == True)[1])
 
 
+    def colors_transform(self, reference_image, input_image):
+        A = self.apply_transform(reference_image, input_image)
 
+        '''
+        A
+        la transformació m'hauria de donar les coordenades d'on se suposa que ve cada pixel de la imatge corregistrada.
+        Aquestes coordenades poden ser decimals i per això el valor del color depèn del color dels 4 pixels més propers.
+        A encara és una matriu
+
+        Abans de res feim uns quantes adaptacions inicials a A. 
+        -No hi ha coordenades negatives per tant qualsevol element negatiu el passam a nes 0.
+        -La coordenada x no pot ser major que les files de la imatge de referència.
+        -La coordenada y no pot ser major que les columnnes de la imatge de referència.
+        '''
+
+        A = np.maximum(A, 0)
+        A[:, :, 0] = np.minimum(A[:, :, 0], reference_image.shape[0] - 1)
+        A[:, :, 1] = np.minimum(A[:, :, 1], reference_image.shape[1] - 1)
+
+        columna1 = A[:, :, 0]
+        columna2 = A[:, :, 1]
+
+        '''
+        A11 A12
+        A21 A22
+
+        per tant:
+         A11: és la x floor i la y ceil      A12: és la x i la y ceil 
+         A21: és la x i la y floor           A22: és la x ceil i la y floor
+        '''
+
+        A11 = np.ones(A.shape)
+        A11[:, :, 0] = np.floor(columna1)
+        A11[:, :, 1] = np.ceil(columna2)
+
+        A12 = np.ceil(A)
+
+        A21 = np.floor(A)
+
+        A22 = np.ones(A.shape)
+        A22[:, :, 0] = np.ceil(columna1)
+        A22[:, :, 1] = np.floor(columna2)
+
+        '''
+        Ara el problema que tenim és que els nombres encara que són enters les tracta com reals i per tant ara passam 
+        totes les matrius a vectors (perquè és més fàcil fer feina amn ells) i a més deim que A11,A12,A21 i A22 són de tipus enters  
+        '''
+        A = imatge_vec(A, 2)
+        A11 = imatge_vec(A11, 2).astype(int)
+        A12 = imatge_vec(A12, 2).astype(int)
+        A21 = imatge_vec(A21, 2).astype(int)
+        A22 = imatge_vec(A22, 2).astype(int)
+
+        '''
+        f11,f12,f21 i f22 són els coeficients per la interpolació bilineal. 
+        és a dir color(x,y) = color(A21(x,y))*f21(x,y) + color(A22(x,y))*f22(x,y) + color(A11(x,y))*f11(x,y) + color(A12(x,y))*f12(x,y)
+
+        així inicialment les possam tots 0 i les anam canviant segons si com sigui (x,y). 
+        '''
+        f11 = np.zeros(A.shape[0])  # només necessitam un nombre per posició
+        f12 = np.zeros(A.shape[0])
+        f21 = np.zeros(A.shape[0])
+        f22 = np.zeros(A.shape[0])
+
+        '''
+        denx,deny,num21x,num21y,num11y,num22x ens serviràn per escriuré més fàcilment qui són f11,f12,f21 i f22 en cada cas
+        '''
+        denx = (A12[:, 0] - A21[:, 0])
+        deny = (A12[:, 1] - A21[:, 1])
+
+        num21x = A12[:, 0] - A[:, 0]
+        num21y = A12[:, 1] - A[:, 1]
+        num11y = A21[:, 1] - A[:, 1]
+        num22x = A21[:, 0] - A[:, 0]
+
+        '''
+        inicialment totes són 0 i segons quina situació estam ho anirem canviant.
+
+        Si la coordenada (x,y) ja eren els dos nombres enters llavors ens trobam al cas (x,y) = (np.ceil(x), np.ceil(y)) 
+        i per tant si volem trobar els pixels que satisfan aquesta condició el que volem és trobar aquelles posicions tals que 
+        A==A21. Això es equivalent als llocs on denx + deny == 0). 
+
+        El cas en què amdbues coordenades són decimals és equivalent a dir denx+deny==2. 
+        per tant en aquesta situació hem de canviar tots els valors.
+
+        Cas x decimal i y entera equival a deny=0 i denx=1. Per tant en aquest cas els afectats són:  deny - denx = -1
+        Cas x enter i y decimal equival a deny=1 i denx=0. Per tant equival a: deny - denx = 1
+        '''
+        f21[np.where(denx + deny == 0)] = 1
+
+        den = np.where(denx + deny == 2, denx * deny, 1)  # per evitar problemes interns dividint entre 0
+        f21[np.where(denx + deny == 2)] = ((num21x * num21y) / den)[np.where(denx + deny == 2)]
+        f22[np.where(denx + deny == 2)] = ((num22x * num21y) / -den)[np.where(denx + deny == 2)]
+        f11[np.where(denx + deny == 2)] = ((num21x * num11y) / -den)[np.where(denx + deny == 2)]
+        f12[np.where(denx + deny == 2)] = ((num22x * num11y) / den)[np.where(denx + deny == 2)]
+
+        denxmodificat = np.where(deny - denx == -1, denx, 1)  # per evitar problemes interns dividint entre 0
+        f21[np.where(deny - denx == -1)] = (num21x / denxmodificat)[np.where(deny - denx == -1)]
+        f22[np.where(deny - denx == -1)] = (num22x / -denxmodificat)[np.where(deny - denx == -1)]
+
+        denymodificat = np.where(deny - denx == 1, deny, 1)  # per evitar problemes interns dividint entre 0
+        f21[np.where(deny - denx == 1)] = (num21y / denymodificat)[np.where(deny - denx == 1)]
+        f11[np.where(deny - denx == 1)] = (num11y / -denymodificat)[np.where(deny - denx == 1)]
+
+        B = (f11 * reference_image[A11[:, 0], A11[:, 1]] + f12 * reference_image[A12[:, 0], A12[:, 1]] + f21 *
+             reference_image[
+                 A21[:, 0], A21[:, 1]] + f22 * reference_image[A22[:, 0], A22[:, 1]])
+
+        '''
+        Això és el que vaig fer inicialment però no és realment interpolació bilineal 
+         # SI TOT FOSSIN NO ENTERS FUNCIONA:
+        f11 = (1 - (-A11[:, 0] + A[:, 0]) * (A11[:, 1] - A[:, 1])) / 3
+        f12 = (1 - (A12[:, 0] - A[:, 0]) * (A12[:, 1] - A[:, 1])) / 3
+        f21 = (1 - (-A21[:, 0] + A[:, 0]) * (-A21[:, 1] + A[:, 1])) / 3
+        f22 = (1 - (A22[:, 0] - A[:, 0]) * (-A22[:, 1] + A[:, 1])) / 3
+
+        #canvis pels enters:
+        # canviam les fij de les posicions on les COLUMNES són enters:
+        f11[np.where(A11[:, 1] == A[:, 1])] = (1 - (-A11[:, 0] + A[:, 0]))[np.where(A11[:, 1] == A[:, 1])]
+        f12[np.where(A11[:, 1] == A[:, 1])] = (1 - (A12[:, 0] - A[:, 0]))[np.where(A11[:, 1] == A[:, 1])]
+        f21[np.where(A11[:, 1] == A[:, 1])] = 0
+        f22[np.where(A11[:, 1] == A[:, 1])] = 0
+
+        # canviam les fij de les posicions on les FILES són enters:
+        f11[np.where(A11[:, 0] == A[:, 0])] = (1 - (A11[:, 1] - A[:, 1]))[np.where(A11[:, 0] == A[:, 0])]
+        f12[np.where(A11[:, 0] == A[:, 0])] = 0
+        f21[np.where(A11[:, 0] == A[:, 0])] = (1 - (-A21[:, 1] + A[:, 1]))[np.where(A11[:, 0] == A[:, 0])]
+        f22[np.where(A11[:, 0] == A[:, 0])] = 0
+
+        # si tant files com columnes són enters:
+        f11[np.where(np.sum(A11 == A, axis=1) == 2)] = 1
+        f12[np.where(np.sum(A11 == A, axis=1) == 2)] = 0
+        f21[np.where(np.sum(A11 == A, axis=1) == 2)] = 0
+        f22[np.where(np.sum(A11 == A, axis=1) == 2)] = 0
+
+        '''
+
+        return B
--- spline_registration/proves/proves_imatge_ca.py
+++ spline_registration/proves/proves_imatge_ca.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.io import imread
+from time import time
 
 
 imatge_input=imread('/Users/mariamagdalenapolpujadas/Desktop/universitat/tfg/GITHUB/spline_registration/proves/dog_input.jpg')
@@ -24,18 +25,54 @@ A2=np.concatenate((A2x,A2y ), axis=0) #array_like with shape (n,) on n= nx*ny*2
 
 #com inicialitzam A2 per a que sigui la identitat tenim: A=A2 són les posicións de la malla a la imatge input també.
 A = np.copy(A2)
-def colors_transform(reference_image, A):
-    '''
-    A
-    la transformació m'hauria de donar les coordenades d'on se suposa que ve cada pixel de la imatge corregistrada.
-    Aquestes coordenades poden ser decimals i per això el valor del color depèn del color dels 4 pixels més propers.
-    A encara és una matriu
+def colors_transform(reference_image,A):
 
-    Abans de res feim uns quantes adaptacions inicials a A.
-    -No hi ha coordenades negatives per tant qualsevol element negatiu el passam a nes 0.
-    -La coordenada x no pot ser major que les files de la imatge de referència.
-    -La coordenada y no pot ser major que les columnnes de la imatge de referència.
     '''
+    #si li introduim A com un array_like with shape (n,) on n= nx*ny*2.
+    #ara A es és una matriu de dimensió (2, nx*ny)
+    # A[0] conté les coordenades x, A[1] conté les coordenades y.
+    '''
+
+    A = np.array([A[0:(nx * ny)], A[nx * ny:(2 * nx * ny)]])
+    A = np.maximum(A, 0)
+    A[0] = np.minimum(A[0], reference_image.shape[0] - 1)
+    A[1] = np.minimum(A[1], reference_image.shape[1] - 1)
+
+
+    X = A[0]
+    Y = A[1]
+    Ux = np.floor(X).astype(int)
+    Vy = np.floor(Y).astype(int)
+
+    a = X-Ux
+    b = Y-Vy
+
+    #si a és 0 no hauriem de tenir en compte Ux+1 tan sols Ux i si b es 0 no he m de tenir en compte Vy+1 tan sols Vy
+    M = np.array([reference_image[Ux, Vy][:, 0],reference_image[Ux, Vy][:, 1],reference_image[Ux, Vy][:, 2]])
+    B = np.array([reference_image[Ux + 1, Vy][:,0],reference_image[Ux + 1, Vy][:,1],reference_image[Ux + 1, Vy][:,2]])
+    C = np.array([reference_image[Ux, Vy + 1][:,0],reference_image[Ux, Vy + 1][:,1],reference_image[Ux, Vy + 1][:,2]])
+    D = np.array([reference_image[Ux+1, Vy+1][:,0],reference_image[Ux+1, Vy+1][:,1],reference_image[Ux+1, Vy+1][:,2]])
+
+
+    color = (a-1)*(b-1)*M + a*(1-b)*B + (1-a)*b*C + a*b*D
+
+    return color
+
+
+'''
+TAMBÉ FUNCIONA PERÒ ÉS MÉS COMPLICAT PER TANT HEM QUED AMB LA VERSIO SIMPLE
+def colors_transform(reference_image, A):
+    
+   # A
+    #la transformació m'hauria de donar les coordenades d'on se suposa que ve cada pixel de la imatge corregistrada.
+    #Aquestes coordenades poden ser decimals i per això el valor del color depèn del color dels 4 pixels més propers.
+    #A encara és una matriu
+
+    #Abans de res feim uns quantes adaptacions inicials a A.
+    #-No hi ha coordenades negatives per tant qualsevol element negatiu el passam a nes 0.
+    #-La coordenada x no pot ser major que les files de la imatge de referència.
+    #-La coordenada y no pot ser major que les columnnes de la imatge de referència.
+    
     A= np.array([A[0:(nx*ny)],A[nx*ny:(2*nx*ny)]])
     #si li introduim A com un array_like with shape (n,) on n= nx*ny*2 feim aquest canvi per emprar el mateix codi.
     #ara A es és una matriu de dimensió (2, nx*ny)
@@ -48,14 +85,13 @@ def colors_transform(reference_image, A):
     columna1 = A[0]
     columna2 = A[1]
 
-    '''
-    A11 A12
-    A21 A22
+    #A11 A12
+    #A21 A22
 
-    per tant:
-     A11: és la x floor i la y ceil      A12: és la x i la y ceil 
-     A21: és la x i la y floor           A22: és la x ceil i la y floor
-    '''
+    #per tant:
+     #A11: és la x floor i la y ceil      A12: és la x i la y ceil 
+     #A21: és la x i la y floor           A22: és la x ceil i la y floor
+
 
     A11 = np.ones(A.shape)
     A11[0] = np.floor(columna1)
@@ -69,22 +105,19 @@ def colors_transform(reference_image, A):
     A22[0] = np.ceil(columna1)
     A22[1] = np.floor(columna2)
 
+    #f11,f12,f21 i f22 són els coeficients per la interpolació bilineal. 
+    #és a dir color(x,y) = color(A21(x,y))*f21(x,y) + color(A22(x,y))*f22(x,y) + color(A11(x,y))*f11(x,y) + color(A12(x,y))*f12(x,y)
 
-    '''
-    f11,f12,f21 i f22 són els coeficients per la interpolació bilineal. 
-    és a dir color(x,y) = color(A21(x,y))*f21(x,y) + color(A22(x,y))*f22(x,y) + color(A11(x,y))*f11(x,y) + color(A12(x,y))*f12(x,y)
+    #així inicialment les possam tots 0 i les anam canviant segons si com sigui (x,y). 
 
-    així inicialment les possam tots 0 i les anam canviant segons si com sigui (x,y). 
-    '''
     # A es (2,400,1)
     f11 = np.zeros(A.shape[1])  # només necessitam un nombre per posició
     f12 = np.zeros(A.shape[1])
     f21 = np.zeros(A.shape[1])
     f22 = np.zeros(A.shape[1])
 
-    '''
-    denx,deny,num21x,num21y,num11y,num22x ens serviràn per escriuré més fàcilment qui són f11,f12,f21 i f22 en cada cas
-    '''
+    #denx,deny,num21x,num21y,num11y,num22x ens serviràn per escriuré més fàcilment qui són f11,f12,f21 i f22 en cada cas
+
     denx = (A12[0] - A21[0])
     deny = (A12[1] - A21[1])
 
@@ -93,19 +126,19 @@ def colors_transform(reference_image, A):
     num11y = A21[1] - A[1]
     num22x = A21[0] - A[0]
 
-    '''
-    inicialment totes són 0 i segons quina situació estam ho anirem canviant.
 
-    Si la coordenada (x,y) ja eren els dos nombres enters llavors ens trobam al cas (x,y) = (np.ceil(x), np.ceil(y)) 
-    i per tant si volem trobar els pixels que satisfan aquesta condició el que volem és trobar aquelles posicions tals que 
-    A==A21. Això es equivalent als llocs on denx + deny == 0). 
+    #inicialment totes són 0 i segons quina situació estam ho anirem canviant.
 
-    El cas en què amdbues coordenades són decimals és equivalent a dir denx+deny==2. 
-    per tant en aquesta situació hem de canviar tots els valors.
+    #Si la coordenada (x,y) ja eren els dos nombres enters llavors ens trobam al cas (x,y) = (np.ceil(x), np.ceil(y)) 
+    #i per tant si volem trobar els pixels que satisfan aquesta condició el que volem és trobar aquelles posicions tals que 
+    #A==A21. Això es equivalent als llocs on denx + deny == 0). 
 
-    Cas x decimal i y entera equival a deny=0 i denx=1. Per tant en aquest cas els afectats són:  deny - denx = -1
-    Cas x enter i y decimal equival a deny=1 i denx=0. Per tant equival a: deny - denx = 1
-    '''
+    #El cas en què amdbues coordenades són decimals és equivalent a dir denx+deny==2. 
+    #per tant en aquesta situació hem de canviar tots els valors.
+
+    #Cas x decimal i y entera equival a deny=0 i denx=1. Per tant en aquest cas els afectats són:  deny - denx = -1
+    #Cas x enter i y decimal equival a deny=1 i denx=0. Per tant equival a: deny - denx = 1
+
     f21[np.where(denx + deny == 0)] = 1
 
     den = np.where(denx + deny == 2, denx * deny, 1)  # per evitar problemes interns dividint entre 0
@@ -134,6 +167,8 @@ def colors_transform(reference_image, A):
          f21 *[a21[:,0],a21[:,1],a21[:,2]]+
          f22 * [a22[:,0],a22[:,1],a22[:,2]])
     return B
+
+'''
 
 imatge_malla_input=colors_transform(imatge_input, A)
 imatge_malla_input=np.hsplit(imatge_malla_input, nx*ny)
@@ -164,6 +199,8 @@ res = least_squares(funcio_minimitzar, A2,method='lm' )
 parametres = res.x
 
 
+malla_x = malla_optima(res.x)[0]
+malla_y = malla_optima(res.x)[1]
 def posicio(x,y):
 
     s = x / delta[0] - np.floor(x / delta[0]) #val 0 quan la x està a coordenadesx
@@ -171,6 +208,8 @@ def posicio(x,y):
     i = np.floor(x / delta[0]).astype(int) #index de la posició més pròxima per davall de la coordenada x a la malla
     j = np.floor(y / delta[1]).astype(int) #index de la posició més pròxima per davall de la coordenada y a la malla
 
+    '''
+    AIXÒ ESTÀ MALAMENT
     xpost = coordenadesx[i + 1]
     xant = coordenadesx[i]
     ypost = coordenadesy[j + 1]
@@ -186,6 +225,13 @@ def posicio(x,y):
         posicioy = malla_y[i,j]
 
     return [posiciox,posicioy]
+    '''
+    A = np.array([malla_x[i, j], malla_y[i, j]])
+    B = np.array([malla_x[i+1, j], malla_y[i+1, j]])
+    C = np.array([malla_x[i, j+1], malla_y[i, j+1]])
+    D = np.array([malla_x[i+1, j+1],malla_y[i+1,j+1]])
+    interpolacio = (s-1)*(t-1)*A + s*(1-t)*B + (1-s)*t*C + s*t*D
+    return interpolacio
 
 
 imatge_transformada = np.zeros_like((imatge_reference.shape[0],imatge_reference.shape[1],2))
